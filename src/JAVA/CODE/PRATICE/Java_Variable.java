127a5e4734bf1409883fd28ab9ef10146f2b7601
VISHAL <vishalnandi14@gmail.com>
2023-10-02 19:45:45 +0530

    commiting try

---
+++ src/JAVA/CODE/PRATICE/Java_Variable.java
@@ -0,0 +1,29 @@
+package JAVA.CODE.PRATICE;
+
+public class Java_Variable {
+    public static void main(String[] args) {
+        int MyNumber;
+        short myshort = 847;
+        long mylong = 98878;
+
+        double mydouble = 7.896;
+        float myfloat= 79.556f;
+
+        char mychar = 'Y';
+        boolean myboolean = true;
+        MyNumber = 88;
+
+        byte mybyte = 127;
+
+        System.out.println(MyNumber);
+        System.out.println(myshort);
+        System.out.println(mylong);
+        System.out.println(mydouble);
+        System.out.println(myfloat);
+        System.out.println(myboolean);
+        System.out.println(mybyte);
+
+
+
+    }
+}
